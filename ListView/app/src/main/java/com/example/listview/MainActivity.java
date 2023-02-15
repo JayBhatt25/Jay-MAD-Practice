a30f96cf3550825e69e4ef00a0a6c4568827b702
Jay Bhatt <jbhatt7@uncc.edu>
2023-02-14 22:30:56 -0500

    ListView generated using ArrayAdapter

--- ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    String[] colors = {"Red", "Green", "Blue"};
+    ArrayList<User> users = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        users.add(new User("Jay Bhatt", 24));
+        users.add(new User("Leo Messi", 37));
+        users.add(new User("Neymar Jr.", 34));
+        users.add(new User("Virat Kohli", 30));
+
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.rootView, new MainFragment())
+                .add(R.id.rootView, MainFragment.newInstance(users))
                 .commit();
     }
 }
